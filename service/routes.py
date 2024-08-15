4f144dfafc5b08b20b41ae90cbf923df1b12dac3
Yasser SAIDI <yasersaidi.ys@gmail.com>
2024-08-15 15:49:25 -0400

    fixing the blank_spaces in the route file 2

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -66,25 +67,32 @@ def list_accounts():
     List all Accounts
     This endpoint will list all Accounts
     """
+    app.logger.info("Request to list Accounts")
+
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
 
     app.logger.info("Returning [%s] accounts", len(account_list))
-    return (jsonify(account_list), status.HTTP_200_OK)
+    return jsonify(account_list), status.HTTP_200_OK
+
 
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-def read_account(account_id):
+def get_accounts(account_id):
     """
-        Reads an Account
-        This endpoint will read an Account based the account_id that is requested
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
     """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    return (account.serialize(), status.HTTP_200_OK)
+
+    return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -104,21 +112,25 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
 
-    return (account.serialize(), status.HTTP_200_OK)
+    return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-def delete_account(account_id):
+def delete_accounts(account_id):
     """
-        Delete an Account
-        This endpoint will delete an Account based on the account_id that is requested
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
     """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+
     account = Account.find(account_id)
     if account:
         account.delete()
-    return ("", status.HTTP_204_NO_CONTENT)
+
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
