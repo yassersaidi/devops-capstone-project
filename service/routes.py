035b5fc668dcbeab0ee31d9e287f75d32ddad38d
Yasser SAIDI <yasersaidi.ys@gmail.com>
2024-08-15 15:37:20 -0400

    updated the routes

--- service/routes.py
+++ service/routes.py
@@ -60,6 +60,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -70,7 +71,7 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
 
     app.logger.info("Returning [%s] accounts", len(account_list))
-    return jsonify(account_list), status.HTTP_200_OK
+    return (jsonify(account_list), status.HTTP_200_OK)
     
 ######################################################################
 # READ AN ACCOUNT
@@ -85,7 +86,7 @@ def read_account(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    return account.serialize(), status.HTTP_200_OK
+    return (account.serialize(), status.HTTP_200_OK)
 
 
 ######################################################################
@@ -106,7 +107,8 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
 
-    return account.serialize(), status.HTTP_200_OK
+    return (account.serialize(), status.HTTP_200_OK)
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -120,7 +122,7 @@ def delete_account(account_id):
     account = Account.find(account_id)
     if account:
         account.delete()
-    return "", status.HTTP_204_NO_CONTENT
+    return ("", status.HTTP_204_NO_CONTENT)
 
 
 ######################################################################
